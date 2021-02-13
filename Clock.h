eac030f72e637db6dbd490e0c0767ff5ffc4a35b
Chris <Davis>
2021-02-13 16:36:52 -0600

    Update for DS3231 in uKenbak-1 kit

--- Clock.h
+++ Clock.h
@@ -13,8 +13,8 @@ public:
   // DS3231 | 0x68 | 0x0E | 0x00
   // DS3232 | 0x68 | 0x0E | 0x14
   static const int RTC_I2C_ADDR         = 0x68;  // ADDR: I2C address, 0x00 means no RTC (no SRAM, use top of EEPROM)
-  static const int RTC_CONTROL_OFFSET   = 0x07;  // CTRL: location of RTC control register, 0x00 means ignore
-  static const int RTC_USER_SRAM_OFFSET = 0x08;  // SRAM: location of first byte of user SRAM in RTC, 0x00 means RTC has no SRAM, use top of EEPROM
+  static const int RTC_CONTROL_OFFSET   = 0x0E;  // CTRL: location of RTC control register, 0x00 means ignore
+  static const int RTC_USER_SRAM_OFFSET = 0x00;  // SRAM: location of first byte of user SRAM in RTC, 0x00 means RTC has no SRAM, use top of EEPROM
 
   Clock();
   void Init();
@@ -30,4 +30,3 @@ private:
 extern Clock clock;
 
 #endif
-
